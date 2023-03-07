cac5d0a448ed315f5f01bb986ef4131191986d26
GabrieldeSouza1 <gabriel.souza.1365691@sga.pucminas.br>
2023-03-06 21:18:43 -0300

    {FIX} Correção de nomes

--- implementacao/app/src/business/Secretaria.java
+++ implementacao/app/src/business/Secretaria.java
@@ -1,11 +1,45 @@
 package business;
 
-public class Secretaria extends Usuario {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Secretaria extends Usuário {
 
 	private int ID_SECRETARIA;
+	private CrudAluno crudAluno;
+	private CrudProfessor crudProfessor;
+	private List<Gerenciavel> usuarios;
 
-	public void criar(Gerenciavel gerenciavel) {
+	public Secretaria(String nome, String senha, int id) {
+		super(nome, senha);
+		this.ID_SECRETARIA = id;
+		usuarios = new ArrayList<Gerenciavel>();
+	}
 
+	public List<Gerenciavel> criar(Gerenciavel gerenciavel) {
+
+		if (gerenciavel.getClass().getTypeName() == "business.Aluno") {
+			if (crudAluno == null) {
+				crudAluno = new CrudAluno();
+			}
+			if(crudAluno.cadastrar(gerenciavel)) {
+				usuarios.add(gerenciavel);
+				return usuarios;
+			}
+			
+		} else {
+			if (gerenciavel.getClass().getTypeName() == "business.Professor") {
+				if (crudProfessor == null) {
+					crudProfessor = new CrudProfessor();
+				}
+				if(crudProfessor.cadastrar(gerenciavel)) {
+					usuarios.add(gerenciavel);
+					return usuarios;
+				}
+				
+			}
+		}
+		return null;
 	}
 
 	public void alterar(Gerenciavel gerenciavel) {
@@ -22,6 +56,10 @@ public class Secretaria extends Usuario {
 
 	public void vincularSemestreAoCurso(Semestre semestre, Curso curso) {
 
+	}
+
+	public List<Gerenciavel> getAllUsers() {
+		return usuarios;
 	}
 
 }
