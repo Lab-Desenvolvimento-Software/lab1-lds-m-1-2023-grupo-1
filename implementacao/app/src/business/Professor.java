ce0961750b93a14911ec3b7aa4c21bf6bc4f4905
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-03-07 17:18:59 -0300

    [ADD] Método getTurma - Professor

--- implementacao/app/src/business/Professor.java
+++ implementacao/app/src/business/Professor.java
@@ -1,18 +1,25 @@
 package business;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Professor extends Usuario implements Gerenciavel {
 
+	private List<Materia> materias;
+
 	public Professor(String nome, String senha) {
 		super(nome, senha);
+		materias = new ArrayList<Materia>();
 	}
 
 	private int ID_PROFESSOR;
 
-	private List<Materia> Materiais;
-
 	public List<Aluno> getTurma(String nome) {
+		for(Materia materia : materias){
+			if((materia.getNome().compareTo(nome))==0){
+				return materia.getAlunos();
+			}
+		}
 		return null;
 	}
 	
