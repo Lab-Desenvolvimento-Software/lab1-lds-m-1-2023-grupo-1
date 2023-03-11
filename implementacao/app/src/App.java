5d56b33b6495ef2489bc965414c183dfb7554d2f
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-03-11 13:10:14 -0300

    [UPDATE] Menu

--- implementacao/app/src/App.java
+++ implementacao/app/src/App.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Array;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.SecureDirectoryStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import business.Aluno;
+import business.CrudMateria;
+import business.CrudProfessor;
 import business.Curso;
 import business.Materia;
 import business.Professor;
@@ -32,6 +35,8 @@ public class App {
         Usuario usuarioLogado = null;
         boolean menuLogin = true;
         boolean menuUsuario = false;
+        boolean menuCadastroSecretaria = true;
+        boolean menuSecretaria = true;
 
         while (menuLogin) {
             System.out.println("Bem-vindo ao sistema de login e registro. Por favor, escolha uma opção:");
@@ -78,7 +83,72 @@ public class App {
                 Secretaria novaSecretaria = new Secretaria(nome, senha);
                 addUsuario(novaSecretaria);
                 usuarios.add(novaSecretaria);
-                System.out.println("Usuário registrado com sucesso!");
+                System.out.println("\nSecretária registrada com sucesso!\n");
+                while(menuSecretaria){
+                    System.out.println("\nEscolha uma opção:");
+                    System.out.println("1. Cadastrar Aluno/Professor");
+                    System.out.println("2. Cadastrar Matéria");
+                    System.out.println("2. Alterar");
+                    System.out.println("3. Excluir");
+                    System.out.println("4. Gerar Currículo");
+                    int escolhaMenu3 = scanner.nextInt();
+                    if(escolhaMenu3 == 1){
+                        while(menuCadastroSecretaria){
+                            menuSecretaria = false;
+                            System.out.println("\nEscolha uma opção de usuário:");
+                            System.out.println("1. Professor");
+                            System.out.println("2. Aluno");
+                            int escolhaMenu2 = scanner.nextInt();
+                            if(escolhaMenu2 == 1){
+                                menuCadastroSecretaria = false;
+                                System.out.println("Digite o nome do professor:");
+                                String nomeProfessor = scanner.next();
+                                System.out.println("Digite a senha do professor:");
+                                String senhaProfessor = scanner.next();
+                                Professor professor = new Professor(nomeProfessor, senhaProfessor);
+                                usuarios.add(professor);
+                                novaSecretaria.criar(professor);
+                                System.out.println("\nProfessor criado com sucesso!");
+                            }
+                            if(escolhaMenu2 == 2){
+                                menuCadastroSecretaria = false;
+                                System.out.println("Digite o nome do aluno:");
+                                String nomeAluno = scanner.next();
+                                System.out.println("Digite a senha do aluno:");
+                                String senhaAluno = scanner.next();
+                                Aluno aluno = new Aluno(nomeAluno, senhaAluno, null);
+                                usuarios.add(aluno);
+                                novaSecretaria.criar(aluno);
+                                System.out.println("\nAluno criado com sucesso!");
+                            }
+                        }
+                    }
+                    if(escolhaMenu3==2){
+                        boolean menuCadastroMateriaSecretaria = true;
+                        while(menuCadastroMateriaSecretaria){
+                            menuSecretaria = false;
+                            System.out.println("\nDigite o nome da matéria:");
+                            String nomeMateria = scanner.next();
+                            System.out.println("\nDigite o preço da matéria:");
+                            double precoMateria = scanner.nextDouble();
+                            System.out.println("A matéria é obrigatória:");
+                            System.out.println("       1. Sim");
+                            System.out.println("       2. Não");
+                            int escolhaObrigatoria = scanner.nextInt();
+                            if(escolhaObrigatoria == 1){
+                                Materia materia = new Materia(nomeMateria, precoMateria, true, true);
+                                novaSecretaria.criar(materia);
+                                System.out.println("Matéria adicionada com sucesso!");
+                            }
+                            if(escolhaObrigatoria == 2){
+                                Materia materia = new Materia(nomeMateria, precoMateria, true, false);
+                                novaSecretaria.criar(materia);
+                                System.out.println("Matéria adicionada com sucesso!");
+                            }
+                        }
+                    }
+                }
+
 
             } else
                 System.out.println("Opção inválida. Tente novamente.");
@@ -93,7 +163,7 @@ public class App {
             while (menuUsuario == true) {
                 System.out.println("");
                 System.out.println("");
-                System.out.println("continua..");
+                System.out.println("secretária..");
                 menuUsuario = false;
             }
         }
@@ -102,15 +172,32 @@ public class App {
             while (menuUsuario == true) {
                 System.out.println("");
                 System.out.println("");
-                System.out.println("continua..");
+                System.out.println("aluno..");
                 menuUsuario = false;
             }
         }
         if (tipoUsuario(usuarioLogado) == "Professor") {
             while (menuUsuario == true) {
-                System.out.println("");
-                System.out.println("");
-                System.out.println("continua..");
+                boolean menuProfessor = true;
+                while(menuProfessor){
+                    System.out.println("Escolha uma opção:");
+                    System.out.println("1. Ver Turma:");
+                    int escolhaMenuProfessor = scanner.nextInt();
+                    if(escolhaMenuProfessor == 1){
+                        menuProfessor = false;
+                        System.out.println("Digite o nome da matéria:");
+                        String escolhaMateria = scanner.next();
+                        Professor professor = new Professor("", "");
+                        List<Aluno> lista = professor.getTurma(escolhaMateria);
+                        try{
+                            for(Aluno aluno: lista){
+                                System.out.println(aluno.getNome());
+                            }
+                        } catch(Exception e){
+                            System.out.println("Não existe uma turma com esse nome!");
+                        }
+                    }
+                }
                 menuUsuario = false;
             }
         } else {
@@ -120,6 +207,10 @@ public class App {
             menuLogin = true;
 
         }
+
+    }
+
+    public void funcoesSecretaria(Secretaria secretaria){
 
     }
 
